736b10c3b88f7263f95c294791e70c424e253fa2
Jason Cope <JCGitNow247@gmail.com>
2025-06-30 20:54:54 -0400

    Rename file

--- shift.login.1024x600.py
+++ shift.login.1024x600.py
@@ -5,42 +5,42 @@ from PIL import Image
 set_appearance_mode('light')
 
 Window = CTk()
-Window.title("Employee Login")
+Window.title("<CompanyName>"+" Powered by TruckBytes")
 Window.iconbitmap("images/our.logos/TruckBytes.ico")
-Window.geometry("640x360")
+Window.geometry("1024x600")
 
 lblTitle = CTkLabel(Window,
                     text="Employee Login",
-                    font=('Arial', 16))
-lblTitle.place(x=430,y=50)
+                    font=('Arial', 32))
+lblTitle.place(x=400,y=50)
 
 lblUsername = CTkLabel(Window,
                        text="Username:", 
-                       font=('Arial',14))
-lblUsername.place(x=120,y=170)
+                       font=('Arial',20))
+lblUsername.place(x=240,y=170)
 
 lblPassword = CTkLabel(Window, 
                        text="Password:", 
-                       font=('Arial',14))
-lblPassword.place(x=120,y=215)
+                       font=('Arial',20))
+lblPassword.place(x=240,y=215)
 
 txtUsernameField = CTkTextbox(Window, width=200,height=1)
-txtUsernameField.place(x=140,y=170)
+txtUsernameField.place(x=412,y=170)
 
 txtPasswordField = CTkTextbox(Window, width=200,height=1)
-txtPasswordField.place(x=140,y=215)
+txtPasswordField.place(x=412,y=215)
 
 btnSubmit = CTkButton(Window,
                       text="Login", 
                       width=200, 
                       height=40)
-btnSubmit.place(x=500,y=270)
+btnSubmit.place(x=412,y=270)
 
 original_logo = Image.open("images/our.logos/TruckBytes.png")
 resized_logo = original_logo.resize((200,200),Image.Resampling.LANCZOS)
 truck_logo = CTkImage(light_image=resized_logo, dark_image=resized_logo, size=(200,200))
 
 imgLogo = CTkLabel(Window,image=truck_logo, text="")
-imgLogo.place(x=500,y=340)
+imgLogo.place(x=412,y=340)
 
 Window.mainloop()
