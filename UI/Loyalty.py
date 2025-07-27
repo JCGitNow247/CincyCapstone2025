90defec7f47f0937d80d73f0f711f77cd22fd04c
FChopin135 <FChopin135@gmail.com>
2025-07-27 19:44:12 -0400

    Fix bug to management tab
    Make sure management tab only ever appears for managers, even upon closing the program without logging out.

--- UI/Loyalty.py
+++ UI/Loyalty.py
@@ -1,10 +1,19 @@
+# Reset login state at startup (ONLY done here)
+import json
+
+# RESET variables upon first entry with the application
+with open("login.json", "w") as f:
+    json.dump({
+        "is_logged_in": False,
+        "employee_id": None,
+        "employee_type_id": None
+    }, f)
+
 #Import TruckBytes Standard UI options
 from OurDisplay import *
 
 #Import to validate email address 
 import re
-
-
 
 #User input validation
 def validate_fields():
